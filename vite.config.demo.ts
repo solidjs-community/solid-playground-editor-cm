89d457f36deb7eab91328ce3698809f7b264a39c
Alex Lohr <alex.lohr@goto.com>
2023-02-16 13:13:35 +0100

    add demo to gh-pages

---
+++ vite.config.demo.ts
@@ -0,0 +1,10 @@
+import solid from "vite-plugin-solid";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [solid()],
+  base: "./",
+  build: {
+    outDir: 'demo',
+  }
+});
